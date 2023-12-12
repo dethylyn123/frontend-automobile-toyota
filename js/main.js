e19239b830186d2edb13807c6b6683fa6b30f358
Dethylyn <dethylyn.b@gmail.com>
2023-12-12 15:27:24 +0800

    Added real time calendar and flash deals

--- js/main.js
+++ js/main.js
@@ -74,6 +74,55 @@
         time: 2000
     });
 
+    // Here starts js code i personally added
+
+     // Latest-news-carousel
+     $(".latest-news-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 2000,
+        center: false,
+        dots: true,
+        loop: true,
+        margin: 25,
+        nav : true,
+        navText : [
+            '<i class="bi bi-arrow-left"></i>',
+            '<i class="bi bi-arrow-right"></i>'
+        ],
+        responsiveClass: true,
+        responsive: {
+            0:{
+                items:1
+            },
+            576:{
+                items:1
+            },
+            768:{
+                items:2
+            },
+            992:{
+                items:3
+            },
+            1200:{
+                items:4
+            }
+        }
+    });
+
+    function updateDate() {
+        var currentDate = new Date();
+        var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+        var formattedDate = currentDate.toLocaleDateString('en-US', options);
+        document.getElementById('currentDate').textContent = 'Date: ' + formattedDate;
+      }
+    
+      // Initial call to set the date when the page loads
+      updateDate();
+    
+      // Update the date every second
+      setInterval(updateDate, 1000);
+
+    // Here ends js code i personally added
 
     // Date and time picker
     $('.date').datetimepicker({
