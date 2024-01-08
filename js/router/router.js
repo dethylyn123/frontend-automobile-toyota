d62e2e6224d8eb47d4d4885cace74de450af5827
Dethylyn <dethylyn.b@gmail.com>
2024-01-09 01:15:26 +0800

    Merged Template and Rename files

---
+++ js/router/router.js
@@ -0,0 +1,30 @@
+function setRouter() {
+    switch (window.location.pathname) {
+        // If you are logged in you cant access outside pages
+        case "/login.html":
+        case "/register.html":
+            if (localStorage.getItem("token")) {
+                window.location.pathname = "/dashboard.html"
+            }
+            break;
+        // If you are not logged in you cant access dashboard pages
+        case "/dashboard.html":
+        // case "/":
+        case "/owners.html":
+        case "/declaration.html":
+        // case "/register.html":
+            if (!localStorage.getItem("token")) {
+                window.location.pathname = "/login.html";
+            }
+            break;
+        case "/users.html":
+            if (localStorage.getItem("role") != "Admin") {
+                window.location.pathname = "/dashboard.html";
+            }
+        default:
+            break;
+
+    }
+}
+
+export {setRouter};
